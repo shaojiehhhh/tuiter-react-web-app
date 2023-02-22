e4ced8533a2af3e4cd474178697b9325201b3f94
shajiehhh <huang.shaoj@northeastern.edu>
2023-02-21 17:28:45 -0800

    A6 lab

---
+++ src/labs/a6/index.js
@@ -0,0 +1,21 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+    from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
+
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+            <ConditionalOutput/>
+            <Classes/>
+            <Styles/>
+            <TodoItem/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Assignment6;
