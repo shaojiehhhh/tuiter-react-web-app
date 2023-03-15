b61e40b8d8bec5cd30cfa91dc2c7b28e6bc0b1ae
shajiehhh <huang.shaoj@northeastern.edu>
2023-03-15 14:49:15 -0700

    A7 lab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
+import Assignment7 from "./labs/a7";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
@@ -10,9 +11,13 @@ function App() {
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route index element={<Labs/>}/>
-                    <Route path="/hello" element={<HelloWorld/>}/>
-                    <Route path="/tuiter/*" element={<Tuiter/>}/>
+                    <Route path="/">
+                        <Route index element={<Labs/>}/>
+                        <Route path="/labs" exact={true} element={<Labs/>}/>
+                        <Route path="/hello" element={<HelloWorld/>}/>
+                        <Route path="/tuiter/*" element={<Tuiter/>}/>
+                        <Route path="/a7" element={<Assignment7/>}/>
+                    </Route>
                 </Routes>
             </div>
         </BrowserRouter>
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Assignment6 from "./a6";
 import Nav from "../nav";
+import Assignment6 from "./a6";
+import Assignment7 from "./a7";
+import {Routes, Route} from "react-router";
 
 function Labs() {
-    return(
+    return (
         <div>
             <Nav/>
-            <Assignment6/>
+            <Routes>
+                <Route index path="/"
+                       element={<Assignment6/>}/>
+                <Route path="/a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
+
 export default Labs;
